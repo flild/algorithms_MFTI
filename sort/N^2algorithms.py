28c0812cb3e5e4d6bce9a85ad85e2fbfe4b7bac7
Flild <flildman@yandex.ru>
2022-08-25 17:51:09 +0300

    сортивка сложностью N^2: вставками, выбором и пузырьком

--- sort/N^2algorithms.py
+++ sort/N^2algorithms.py
@@ -1,5 +1,3 @@
-
-
 def insert_sort(mass):
     '''
     Сортировка массива вставками по возрастанию
@@ -8,12 +6,11 @@ def insert_sort(mass):
     :return:
         None, так как менеяет исходный массив
     '''
-    for top in range(1,len(mass)):
+    for top in range(1, len(mass)):
         cur = top
-        while cur>0 and mass[cur]<mass[cur-1]:
-            mass[cur], mass[cur-1] = mass[cur-1], mass[cur]
-            cur-=1
-
+        while cur > 0 and mass[cur] < mass[cur - 1]:
+            mass[cur], mass[cur - 1] = mass[cur - 1], mass[cur]
+            cur -= 1
 
 
 def choise_sort(mass):
@@ -25,8 +22,8 @@ def choise_sort(mass):
         None
         Меняет исходный массив
     '''
-    for pos in range(0,len(mass)-1):
-        for el in range(pos+1,len(mass)):
+    for pos in range(0, len(mass) - 1):
+        for el in range(pos + 1, len(mass)):
             if mass[el] < mass[pos]:
                 mass[el], mass[pos] = mass[pos], mass[el]
 
@@ -39,26 +36,29 @@ def bubble_sort(mass):
     :return:
         None, меняет исходный массив
     '''
-    for bypass in range(1,len(mass)-1):
-        for el2 in range(0,len(mass)-bypass):
-            if mass[el2]>mass[el2+1]:
-                mass[el2], mass[el2+1] = mass[el2+1], mass[el2]
+    for bypass in range(1, len(mass) - 1):
+        for el2 in range(0, len(mass) - bypass):
+            if mass[el2] > mass[el2 + 1]:
+                mass[el2], mass[el2 + 1] = mass[el2 + 1], mass[el2]
 
 
 def test1_insert_sort():
-    mass = [4,2,5,1,3]
+    mass = [4, 2, 5, 1, 3]
     insert_sort(mass)
-    assert mass == [1,2,3,4,5]
+    assert mass == [1, 2, 3, 4, 5]
+
 
 def test2_choise_sort():
     mass = [4, 2, 5, 1, 3]
     choise_sort(mass)
     assert mass == [1, 2, 3, 4, 5]
 
+
 def test3_bubble_sort():
     mass = [4, 2, 5, 1, 3]
     bubble_sort(mass)
     assert mass == [1, 2, 3, 4, 5]
+
 
 if __name__ == '__main__':
     pass
