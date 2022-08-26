8299ff1cad8d1b75cdfc82f4251ee06ea5254c2d
Flild <flildman@yandex.ru>
2022-08-26 14:54:39 +0300

    Генерирует все варианты перестановок цифр до N (с лидирующими незначищами нулями) длины М

---
+++ recursion/generate_number.py
@@ -0,0 +1,23 @@
+import pprint
+
+def generate_number(N: int, M: int, prefix=None):
+    '''
+    Генерирует все варианты перестановок цифр до N (с лидирующими незначищами нулями)
+    длины М
+    :param N: Колическтво чисел для перестановки, N<10
+    :param M: Длина чисел на выходе
+    :param prefix: Нужен для рекурсивной генирации, изначально None
+    :return: Список всех чисел
+    '''
+    prefix = prefix or []
+    if M == 0:
+        print(prefix)
+        return
+    for digit in range(N):
+        if digit in prefix:
+            continue
+        prefix.append(digit)
+        generate_number(N,M-1,prefix)
+        prefix.pop()
+
+generate_number(8,8)
